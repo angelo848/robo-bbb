d2f0b15b7de90ca8e6f008f94e32d4750daaa182
Ângelo Salles <angelo_848@live.com>
2021-02-23 19:27:16 -0300

    nome de variavel alterado

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const bot = async () => {
     waitUntil: 'domcontentloaded'
   });
   await page.waitForSelector(DIVS_SELECTOR)
-  const divCarol = await page.$$eval(DIVS_SELECTOR, async (elements) => {
+  const selectedParticipant = await page.$$eval(DIVS_SELECTOR, async (elements) => {
     return elements.filter(element => {
       const IMAGESRC = "https://s2.glbimg.com/VgrPNyV0OQz2HAFSPVBXOC1g6Ek=/0x0:650x650/648x0/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2021/y/y/IuCvs3TkCCl8BAK9iLqQ/karol-conka.jpg"
       const position = getComputedStyle(element).getPropertyValue('position')
@@ -27,10 +27,10 @@ const bot = async () => {
       }
       return false
     })
-    .map(element => element.className)
+      .map(element => element.className)
   })
 
-  const name = await page.$(`.${divCarol[0]} ${NAME_SELECTOR}`)
+  const name = await page.$(`.${selectedParticipant[0]} ${NAME_SELECTOR}`)
   await name.click()
   await page.waitForTimeout(500)
 
